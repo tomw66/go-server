9dc7bbd33bba0bc5b9bde036fbbd2b4191c88575
Tom Wilson <t.wilson6@exeter.ac.uk>
2024-12-13 13:38:25 +0000

    initial commit

---
+++ cmd/webserver/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		log.Fatalf("problem creating file system player store, %v ", err)
+	}
+	
+	server := poker.NewPlayerServer(store)
+
+	if err := http.ListenAndServe(":3000", server); err != nil {
+		log.Fatalf("could not listen on port 3000 %v", err)
+	}
+}
