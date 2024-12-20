a3092bdaa67e82cace965814a9eb12c9eb582c2c
Tom Wilson <t.wilson6@exeter.ac.uk>
2024-12-20 17:09:49 +0000

    NewCLI function

--- CLI_test.go
+++ CLI_test.go
@@ -11,19 +11,19 @@ func TestCLI(t *testing.T) {
 
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
-		playerStore := &poker.StubPlayerStore{}
+		playerStore :=  &poker.StubPlayerStore{}
 
-		cli := &CLI{playerStore, in}
+		cli := &poker.CLI{playerStore, in}
 		cli.PlayPoker()
 
-		assertPlayerWin(t, playerStore, "Chris")
+		poker.assertPlayerWin(t, playerStore, "Chris")
 	})
 
 	t.Run("record cleo win from user input", func(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
-		playerStore := &StubPlayerStore{}
+		playerStore := &poker.StubPlayerStore{}
 
-		cli := &CLI{playerStore, in}
+		cli := &poker.CLI{playerStore, in}
 		cli.PlayPoker()
 
 		poker.assertPlayerWin(t, playerStore, "Cleo")
